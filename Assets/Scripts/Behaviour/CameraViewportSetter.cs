c8236ff6ef68d0a6cdd45dfeb3cbd8cb56be88d9
KonH <konh@yandex.ru>
2020-10-03 20:47:47 +0700

    Add glitch sample

---
+++ Assets/Scripts/Behaviour/CameraViewportSetter.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public sealed class CameraViewportSetter : MonoBehaviour {
+	public RectTransform Target;
+
+	public void Start() {
+		var width = Target.rect.width;
+		var ratio = width / Screen.width;
+		var cam   = GetComponent<Camera>();
+		var rect  = cam.rect;
+		rect.x     = (1 - ratio) / 2;
+		rect.width = ratio;
+		cam.rect   = rect;
+	}
+}
