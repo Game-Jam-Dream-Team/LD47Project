e7ac2b589de136b2638e96c257d70e74f822c7dc
KonH <konh@yandex.ru>
2020-10-03 09:55:39 +0700

    Setup server build pipeline

--- BuildPipeline/Build.cs
+++ BuildPipeline/Build.cs
@@ -1,8 +1,13 @@
+using System;
 using System.IO;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.Execution;
+using Nuke.Common.IO;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [UnsetVisualStudioEnvironmentVariables]
 class Build : NukeBuild {
@@ -31,6 +36,86 @@ class Build : NukeBuild {
 			proc.WaitForExit();
 		});
 
+	const string ServerProject = "Server";
+
+	Target CleanServer => _ => _
+		.Executes(() => {
+			DotNetClean(new DotNetCleanSettings()
+				.SetProject(ServerProject)
+				.SetConfiguration(Configuration.Release));
+		});
+
+	Target RestoreServer => _ => _
+		.Executes(() => {
+			DotNetRestore(new DotNetRestoreSettings()
+				.SetProjectFile(ServerProject));
+		});
+
+	Target CompileServer => _ => _
+		.DependsOn(CleanServer, RestoreServer)
+		.Executes(() => {
+			DotNetBuild(new DotNetBuildSettings()
+				.SetProjectFile(ServerProject)
+				.SetConfiguration(Configuration.Release));
+		});
+
+	Target PublishServer => _ => _
+		.DependsOn(CompileServer)
+		.Executes(() => {
+				var settings = new DotNetPublishSettings()
+					.SetProject(ServerProject)
+					.SetConfiguration(Configuration.Release)
+					.SetRuntime("linux-arm")
+					.SetSelfContained(true)
+					.SetArgumentConfigurator(a => a.Add("/p:PublishSingleFile=true"));
+				DotNetPublish(settings);
+			});
+
+	[Parameter]
+	public string LocalPiHome;
+
+	Target DeployServer => _ => _
+		.Description("Deploy server to target Pi directory root")
+		.Requires(() => LocalPiHome)
+		.DependsOn(PublishServer)
+		.Executes(() => {
+			var buildConfigurationDir = RootDirectory / ServerProject / "bin" / "Release";
+			var buildDir              = GetBuildDir(buildConfigurationDir);
+			var targetPath            = (AbsolutePath) LocalPiHome / ServerProject;
+			var sourceDirPath         = buildDir / "linux-arm" / "publish";
+			CopyDirectoryRecursively(sourceDirPath, targetPath,
+				DirectoryExistsPolicy.Merge, FileExistsPolicy.OverwriteIfNewer);
+		});
+
+	[Parameter]
+	public string SshHost;
+
+	[Parameter]
+	public string SshUserName;
+
+	[Parameter]
+	public string SshPassword;
+
+	Target StopService => _ => _
+		.Description("Stop server service")
+		.Requires(() => SshHost)
+		.Requires(() => SshUserName)
+		.Requires(() => SshPassword)
+		.Executes(() => {
+			var context = new ServiceTarget.ExecutionContext(SshHost, SshUserName, SshPassword);
+			ServiceTarget.StopService(context, "ld47server");
+		});
+
+	Target StartService => _ => _
+		.Description("Start server service")
+		.Requires(() => SshHost)
+		.Requires(() => SshUserName)
+		.Requires(() => SshPassword)
+		.Executes(() => {
+			var context = new ServiceTarget.ExecutionContext(SshHost, SshUserName, SshPassword);
+			ServiceTarget.StartService(context, "ld47server");
+		});
+
 	string GetUnityPath() =>
 		EnvironmentInfo.IsWin
 			? "C:/Program Files/Unity/Hub/Editor/2020.1.6f1/Editor/Unity.exe"
@@ -55,5 +140,17 @@ class Build : NukeBuild {
 		var proc = ProcessTasks.StartProcess("git", "rev-parse --short HEAD");
 		proc.WaitForExit();
 		return proc.Output.First().Text;
+	}
+
+	static AbsolutePath GetBuildDir(AbsolutePath buildConfigurationDir) {
+		var frameworkDirs = Directory.GetDirectories(buildConfigurationDir);
+		if ( (frameworkDirs.Length == 0) ) {
+			throw new InvalidOperationException($"No framework directories found at '{buildConfigurationDir}'");
+		}
+		if ( (frameworkDirs.Length > 1) ) {
+			throw new InvalidOperationException(
+				$"More than one framework directories found at '{buildConfigurationDir}'");
+		}
+		return (AbsolutePath) frameworkDirs[0];
 	}
 }
