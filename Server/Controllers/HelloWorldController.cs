421a248d28281c4d52bdfd7014be7eb790b60ae1
KonH <konh@yandex.ru>
2020-10-03 09:34:17 +0700

    Add hello world server

---
+++ Server/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Server.Controllers {
+	[ApiController]
+	[Route("[controller]")]
+	public sealed class HelloWorldController : ControllerBase {
+		readonly ILogger _logger;
+
+		public HelloWorldController(ILogger<HelloWorldController> logger) {
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public string Get() {
+			_logger.LogInformation("Called");
+			return "Hello World!";
+		}
+	}
+}
