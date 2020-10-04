4912153c4efa642bf9673f93553b2d56a72b8c50
KonH <konh@yandex.ru>
2020-10-04 15:44:01 +0700

    Add slide show support

--- Assets/Scripts/Behaviour/ImageView.cs
+++ Assets/Scripts/Behaviour/ImageView.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Game.Common;
 using Game.State;
 using UnityEngine;
@@ -9,6 +11,12 @@ namespace Game.Behaviour {
 		public Image      Image;
 
 		TweetSpritesCollection _tweetSpritesCollection;
+
+		List<TweetSpritesCollection.TimedSprite> _sprites;
+
+		int   _index;
+		float _timer;
+		float _delay;
 
 		void Awake() {
 			Close();
@@ -23,19 +31,56 @@ namespace Game.Behaviour {
 			GameState.Instance.TweetsController.ImageClick -= OnImageClick;
 		}
 
+		void Update() {
+			if ( _sprites == null ) {
+				return;
+			}
+			_timer += Time.deltaTime;
+			if ( _timer < _delay ) {
+				return;
+			}
+			_timer = 0;
+			CycleSprites();
+		}
+
+		void CycleSprites() {
+			_index++;
+			if ( _index >= _sprites.Count ) {
+				_sprites = null;
+				Close();
+				return;
+			}
+			SetupTimedSprite(_index);
+		}
+
 		void OnImageClick(Tweet tweet) {
 			Open(tweet);
 		}
 
 		void Open(Tweet tweet) {
 			Background.SetActive(true);
-			Image.sprite  = _tweetSpritesCollection.GetTweetSprite(tweet.ImageId);
+			_sprites = _tweetSpritesCollection.GetTweetSprites(tweet.ImageId);
+			if ( _sprites.Count > 0 ) {
+				_index = 0;
+				_timer = 0;
+				SetupTimedSprite(_index);
+			} else {
+				Image.sprite = _tweetSpritesCollection.GetTweetSprite(tweet.ImageId);
+			}
 			Image.enabled = true;
 		}
 
 		public void Close() {
+			if ( _sprites != null ) {
+				return;
+			}
 			Background.SetActive(false);
 			Image.enabled = false;
+		}
+
+		void SetupTimedSprite(int index) {
+			Image.sprite = _sprites[index].Sprite;
+			_delay       = _sprites[index].Time;
 		}
 	}
 }
--- Assets/Scripts/Behaviour/TweetSpritesCollection.cs
+++ Assets/Scripts/Behaviour/TweetSpritesCollection.cs
@@ -7,9 +7,16 @@ namespace Game.Behaviour {
 	[CreateAssetMenu(menuName = "Create TweetSpritesCollection")]
 	public sealed class TweetSpritesCollection : ScriptableObject {
 		[Serializable]
-		public sealed class TweetSpriteInfo {
-			public int    Id;
+		public sealed class TimedSprite {
 			public Sprite Sprite;
+			public float  Time;
+		}
+
+		[Serializable]
+		public sealed class TweetSpriteInfo {
+			public int               Id;
+			public Sprite            Sprite;
+			public List<TimedSprite> Sprites;
 		}
 
 		public List<TweetSpriteInfo> TweetSpriteInfos = new List<TweetSpriteInfo>();
@@ -18,6 +25,16 @@ namespace Game.Behaviour {
 			foreach ( var tweetSpriteInfo in TweetSpriteInfos ) {
 				if ( tweetSpriteInfo.Id == tweetSpriteId ) {
 					return tweetSpriteInfo.Sprite;
+				}
+			}
+			Debug.LogErrorFormat("Can't find TweetSpriteInfo for id '{0}'", tweetSpriteId);
+			return null;
+		}
+
+		public List<TimedSprite> GetTweetSprites(int tweetSpriteId) {
+			foreach ( var tweetSpriteInfo in TweetSpriteInfos ) {
+				if ( tweetSpriteInfo.Id == tweetSpriteId ) {
+					return tweetSpriteInfo.Sprites;
 				}
 			}
 			Debug.LogErrorFormat("Can't find TweetSpriteInfo for id '{0}'", tweetSpriteId);
