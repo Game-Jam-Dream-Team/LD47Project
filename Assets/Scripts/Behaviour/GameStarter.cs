7457c0d76ef824b5692f90225d98bfe6d596f914
KonH <konh@yandex.ru>
2020-10-05 02:25:38 +0700

    Remove deprecated component

--- Assets/Scripts/Behaviour/GameStarter.cs
+++ Assets/Scripts/Behaviour/GameStarter.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 namespace Game.Behaviour {
 	public sealed class GameStarter : MonoBehaviour {
-		public TweetsFeedView TweetsFeedView;
-
 		void OnEnable() {
 			GameState.EnsureExists();
 		}
