fb1a3d6c58c6b550803601b56d926667678a5100
KonH <konh@yandex.ru>
2020-10-05 03:27:41 +0700

    Add settings window with placeholder logic

---
+++ Assets/Scripts/Behaviour/SettingsWindow.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Behaviour {
+	public class SettingsWindow : MonoBehaviour {
+		public GameObject     AgeBlock;
+		public TMP_InputField AgeInput;
+		public Button         SettingsButton;
+		public Button         BackButton;
+
+		void Awake() {
+			gameObject.SetActive(false);
+			AgeBlock.SetActive(false);
+			SettingsButton.onClick.AddListener(Show);
+			BackButton.onClick.AddListener(Hide);
+			AgeInput.onValueChanged.AddListener(OnAgeChanged);
+		}
+
+		void Show() {
+			gameObject.SetActive(true);
+			AgeBlock.SetActive(IsAgeShouldBeShown());
+		}
+
+		void Hide() {
+			gameObject.SetActive(false);
+		}
+
+		bool IsAgeShouldBeShown() {
+			// TODO: check condition
+			return false;
+		}
+
+		void OnAgeChanged(string value) {
+			if ( !int.TryParse(value, out var intValue) ) {
+				return;
+			}
+			if ( intValue >= 99 ) {
+				// TODO: condition met
+			}
+		}
+	}
+}
