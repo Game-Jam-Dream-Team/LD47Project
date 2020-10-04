60d1a999c1d0d79e39586ab9cffbe2d8b9fd1cdf
KonH <konh@yandex.ru>
2020-10-04 17:07:12 +0700

    Fix incorrect viewport change

--- Assets/Scripts/Behaviour/CameraViewportSetter.cs
+++ Assets/Scripts/Behaviour/CameraViewportSetter.cs
@@ -3,13 +3,10 @@ using UnityEngine;
 namespace Game.Behaviour {
 	[RequireComponent(typeof(Camera))]
 	public sealed class CameraViewportSetter : MonoBehaviour {
-		public RectTransform Target;
-
 		public void Start() {
-			var width = Target.rect.width;
-			var ratio = width / Screen.width;
 			var cam   = GetComponent<Camera>();
 			var rect  = cam.rect;
+			var ratio = 600.0f / 1920;
 			rect.x     = (1 - ratio) / 2;
 			rect.width = ratio;
 			cam.rect   = rect;
